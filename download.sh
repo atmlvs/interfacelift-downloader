1ee2c53d6654745c4e2b6ca77a3ad7c614f3e1b5
Ubuntu <ubuntu@ip-172-31-14-246.ap-northeast-2.compute.internal>
2017-07-05 13:53:58 +0000

    add shell script

---
+++ download.sh
@@ -0,0 +1,3 @@
+python interfacelift-downloader.py 2560x1600 -t 10 -d wallpapers_2560x1600 >interfacelift.log 2>&1 &
+python interfacelift-downloader.py 1920x1080 -t 10 -d wallpapers_1920x1080 >interfacelift.log 2>&1 &
+python interfacelift-downloader.py 3840x2160 -t 10 -d wallpapers_3840x2160 >interfacelift.log 2>&1 &
---
+++ sync.sh
@@ -0,0 +1,3 @@
+s3cmd sync wallpapers_2560x1600 s3://11-image-resource/ >/dev/null &
+s3cmd sync wallpapers_1920x1080 s3://11-image-resource/ >/dev/null &
+s3cmd sync wallpapers_3840x2160 s3://11-image-resource/ >/dev/null &
